e8b379ff58a53b95c0bd610a3a44373f6a987282
dhkwak <donghyeok.kwak@interxlab.com>
2022-07-18 13:53:02 +0900

    cp/cpk

--- PanasiaXbar/Pages/Index.razor.cs
+++ PanasiaXbar/Pages/Index.razor.cs
@@ -7,10 +7,14 @@ namespace PanasiaXbar.Pages
 {
     public partial class Index
     {
-        private int _subGroupCount = 10;
+        private int _subGroupCount = 10; //서브그룹(측정한 시료의 개수)
 
         public List<XbarRangeModel> XbarRangeModels { get; set; }
-        
+        public decimal? Cp { get; set; } = null;
+        public decimal? Cpk { get; set; } = null;
+        private decimal _avg;
+        private decimal _stddev;
+
 
         [Inject]
         public IJSRuntime JS { get; set; }
@@ -20,7 +24,12 @@ namespace PanasiaXbar.Pages
             string text = System.IO.File.ReadAllText(@"Data\data.json");
             List<RawDataModel> data = JsonSerializer.Deserialize<List<RawDataModel>>(text);
 
+            //Xbar-R chart 데이터
             XbarRangeModels = GetXbarRangeModels(data);
+
+            //cp/cpk 
+            Cp = Math.Round(GetCp(data), 3);
+            Cpk = Math.Round(GetCpk(), 3);
         }
 
         protected override void OnAfterRender(bool firstRender)
@@ -81,6 +90,26 @@ namespace PanasiaXbar.Pages
             return xbarRangeModels;
         }
 
+        private decimal GetCp(List<RawDataModel> rawDataModels)
+        {
+            //표준편차
+            _avg = rawDataModels.Select(x => x.VALUE).Average();
+            var sdSum = rawDataModels.Select(x => (x.VALUE - _avg) * (x.VALUE - _avg)).Sum();
+            _stddev = Convert.ToDecimal(Math.Sqrt(Convert.ToDouble(sdSum / rawDataModels.Count)));
+            //cp 
+            var cp = (Spec.Usl - Spec.Lsl) / (6 * _stddev);
+            return cp;
+        }
+
+        private decimal GetCpk()
+        {
+            var val1 = Math.Abs(((Spec.Usl + Spec.Lsl) / 2) - _avg);
+            var val2 = (Spec.Usl - Spec.Lsl) / 2;
+            var k = val1 / val2; //k(치우침)
+            var cpk = (1 - k) * Convert.ToDecimal(Cp);
+            return cpk;
+        }
+
     }
     public class RawDataModel
     {
@@ -121,7 +150,14 @@ namespace PanasiaXbar.Pages
         public decimal RangeUcl { get; set; }
         public decimal RangeCl { get; set; }
         public decimal RangeLcl { get; set; }
-
     }
+
+    //cp/cpk 
+    public static class Spec
+    {
+        public static decimal Usl { get; set; } = 3.9M; //임의로 지정함
+        public static decimal Lsl { get; set; } = 3.6M; //임의로 지정함
+    }
+
 }
 
